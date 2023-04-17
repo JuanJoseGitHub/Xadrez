5666ca9d0e7fcea8758010bfa5051051b78ce8fe
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-04-17 20:49:54 +0200

    Taboleiro

--- src/componentes/Taboleiro.jsx
+++ src/componentes/Taboleiro.jsx
@@ -1,30 +1,27 @@
 import React from 'react'
-
+import styles from '../css/Taboleiro.module.css'
 export default function Taboleiro() {
   
     return (
-    <div>
-        <div>
-            <div className='b' id='a8'>a8</div>
-            <div className='n' id='b8'>b8</div>
-            <div className='b' id='c8'>c8</div>
-            <div className='n' id='d8'>d8</div>
-            <div className='b' id='e8'>e8</div>
-            <div className='n' id='f8'></div>
-            <div className='b' id='g8'></div>
-            <div className='n' id='h8'></div>
-        </div>
-        <div>
-            <div className='n' id='a7'>a7</div>
-            <div className='b' id='b7'>b7</div>
-            <div className='n' id='c7'></div>
-            <div className='b' id='d7'></div>
-            <div className='n' id='e7'></div>
-            <div className='b' id='f7'></div>
-            <div className='n' id='g7'></div>
-            <div className='b' id='h7'></div>
-        </div>
-
+    <div id={styles.taboleiro}>
+            <div className={styles.b} id={styles.a8}>a8</div>
+            <div className={styles.n} id={styles.a7}>a7</div>
+            <div className={styles.b} id={styles.a6}>a6</div>
+            <div className={styles.n} id={styles.a5}>a5</div>
+            <div className={styles.b} id={styles.a4}>a4</div>
+            <div className={styles.n} id={styles.a3}>a3</div>
+            <div className={styles.b} id={styles.a2}>a2</div>
+            <div className={styles.n} id={styles.a1}>a1</div>
+            
+            <div className={styles.n} id='b8'>b8</div>
+            <div className={styles.b} id='b7'>b7</div>
+            <div className={styles.n} id='b6'>b6</div>
+            <div className={styles.b} id='b5'>b5</div>
+            <div className={styles.n} id='b4'>b4</div>
+            <div className={styles.b} id='b3'>b3</div>
+            <div className={styles.n} id='b2'>b2</div>
+            <div className={styles.b} id='b1'>b1</div>
+            
     </div>
     )
 }
