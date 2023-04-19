e3073ee950fda6d3da9d8cfa9093ba2b45b515e0
jjhidalgoneira <xxhidalgoneira@gmail.com>
2023-04-19 22:02:01 +0200

    Optimizando componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import TaboleiroInicio from './componentes/TaboleiroInicio.jsx'
-import LeeTabASCII from './componentes/LeeTabASCII';
 import Taboleiro from './componentes/Taboleiro';
 
 function App() {
   return (
     <>
-      {/* <TaboleiroInicio></TaboleiroInicio> */}
-      {/* <LeeTabASCII /> */}
       <Taboleiro></Taboleiro>
     </>
-  );
+  )
 }
 
 export default App;
