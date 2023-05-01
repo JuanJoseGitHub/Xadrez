27e257d57aeb88df85b9283be8053abb843b6730
jjhidalgoneira <xxhidalgoneira@gmail.com>
2023-05-01 17:19:42 +0200

    Ficheiros PGN

--- backend/src/main.mjs
+++ backend/src/main.mjs
@@ -6,3 +6,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/XadrezAPI/",(peticion,resposta)=>{
+    resposta.status(200)
+    resposta.send("Benvido á API de xadrez")
+})
+
+app.listen(3000,()=>{console.log("[Escoitando]");})
--- frontend/src/componentes/FicheirosPGN.jsx
+++ frontend/src/componentes/FicheirosPGN.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 export default function FicheirosPGN() {
+  
+  function cargaPGN () {
+    let partidaAcargar="1.e4 e5"
+    const resposta= fetch ("/XadrezAPI/", 
+    {
+      method:'POST',
+      header:{'Content-type':'applcation/json'},
+      body: JSON.stringify (
+        { partida: partidaAcargar})
+
+    })
+
+  } 
+ 
   return (
+    <>
     <div>FicheirosPGN</div>
+    <button onClick={cargaPGN}></button>
+    </>
   )
 }
