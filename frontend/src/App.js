cdfd9d7870efbd73d9e80c9b5d3090059d8c3cc7
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-06-12 17:01:24 +0200

    Arranxo GIT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,34 +30,7 @@ function App() {
   
   return (
     <>
-    <Avatares/>
-    {/* <Contexto.Provider value={{stateEmpate, statePartidaenPGN}}> 
-      
-      <BrowserRouter>
-
-      <nav className='enlaces'>
-          <Link className='link' to="/">| Xadrez 64 |</Link>
-          <Link className='link' to="/xogar">| Xogar unha partida |</Link>      
-          <Link className='link' to="/verlibro">| Ver Libro |</Link>
-          <Link className='link' to="/verpartida">| Ver Partida 1 |</Link>
-          <Link className='link' to="/verpartidas">| Ver Partidas |</Link>
-          <Link className='link' to="/borra">| Borrar Partidas |</Link>
-          <Link className='link' to="/cambia">| Cambiar |</Link>
-      </nav>
-
-      <Routes className='enlaces'>
-        <Route path='/' element={<Home></Home>}/>
-        <Route path='/xogar' element={<LeeXogada></LeeXogada>}/>
-        <Route path='/verlibro/' element={<VerLibro></VerLibro>}/>
-        <Route path='/verpartida/' element={<VerPartida></VerPartida>}/>
-        <Route path='/verpartidas/' element={<SeleccionaPGN></SeleccionaPGN>}/>
-        <Route path='/borra/' element={<BorraPGN></BorraPGN>}/>
-        <Route path='/cambia/' element={<CambiaCabeceiraPGN></CambiaCabeceiraPGN>}/>
-      </Routes>   
-
-      </BrowserRouter>
-    </Contexto.Provider> */}
-
+    
       {/* PartidaAuto: Reproduce automaticamente unha partida que se suministra nun Array de strings sen numerar*/}
       {/* autoPartida=["d4","Nf6","Bf4","c5","e3","Qb6","Nc3","Qxb2","Nb5","cxd4","Rb1","Qxa2","Nc7+","Kd8","Nxa8","e5","Bg5","Qa5+","Qd2","Qa2","Rd1","Qb2","c3","dxc3","Qxb2","cxb2","Bc4","Bb4+","Ke2","b6","Nf3","Re8","Bxf7","Ba6+","Rd3","Re7","Ba2","e4","Nd2","exd3+","Kd1","Bb7","Nxb6","axb6","f3","Re5","Bxf6+","gxf6","Nc4","Rc5","Nxb2","Ba6","Bb1","d2"] */}
       {/* <PartidaAuto autoPartida={["d4","Nf6","Bf4","c5","e3","Qb6","Nc3","Qxb2","Nb5","cxd4","Rb1","Qxa2","Nc7+","Kd8","Nxa8","e5","Bg5","Qa5+","Qd2","Qa2","Rd1","Qb2","c3","dxc3","Qxb2","cxb2","Bc4","Bb4+","Ke2","b6","Nf3","Re8","Bxf7","Ba6+","Rd3","Re7","Ba2","e4","Nd2","exd3+","Kd1","Bb7","Nxb6","axb6","f3","Re5","Bxf6+","gxf6","Nc4","Rc5","Nxb2","Ba6","Bb1","d2"]} ></PartidaAuto> */}
@@ -79,23 +52,20 @@ function App() {
       {/* Mostra a partida almacenada con id=1 automáticamente ou xogada a xogada */}
       {/* {<VerPartida></VerPartida>} */}
 
-       {/* Mostra as partidas alamcenadas en base de datos e permite reproducir unha xogada a xogada */}
+      {/* Mostra as partidas alamcenadas en base de datos e permite reproducir unha xogada a xogada */}
       {/* <SeleccionaPGN/> */}
 
       {/* Mostra as partidas almacenadas en base de datos e permite seleccionar unha para borrar */}
       {/* <BorraPGN></BorraPGN> */}
 
       {/* <CargaPGN/> */}
-
-
-
-    <Avatares/>
-    <Contexto.Provider value={{stateEmpate, statePartidaenPGN}}> 
+   
+    <Contexto.Provider value={{stateEmpate, statePartidaenPGN, stateEvent, stateSite, stateDate, stateRound, stateWhite, stateBlack, stateResult, stateBrancas, stateNegras}}> 
       
       <BrowserRouter>
 
       <nav className='enlaces'>
-          <Link className='link' to="/">| Xadrez 64 |</Link>
+          <Link className='link' to="/">| Inicio |</Link>
           <Link className='link' to="/xogar">| Xogar unha partida |</Link>      
           <Link className='link' to="/verlibro">| Ver Libro |</Link>
           <Link className='link' to="/verpartida">| Ver Partida 1 |</Link>
@@ -105,7 +75,7 @@ function App() {
       </nav>
 
       <Routes className='enlaces'>
-        <Route path='/' element={<Home></Home>}/>
+        <Route path='/' element={<Avatares></Avatares>}/>
         <Route path='/xogar' element={<LeeXogada></LeeXogada>}/>
         <Route path='/verlibro/' element={<VerLibro></VerLibro>}/>
         <Route path='/verpartida/' element={<VerPartida></VerPartida>}/>
--- frontend/src/componentes/Avatares.jsx
+++ frontend/src/componentes/Avatares.jsx
@@ -1,33 +1,83 @@
-import React, { useEffect } from 'react'
-import style from '../css/VerLibro.module.css'
-import { useState } from 'react'
-
-// Cargar avatares (xogadorA, xogadorB)
+import React from 'react'
+import style from '../css/avatares.module.css'
+import { useContext} from 'react'
+import { Contexto } from "../App";
 
 export default function Avatares() {
 
-    const [file, setFile] = useState()
-    
-    useEffect(
-        ()=>{ 
-          console.log(file)
-          
-         },
-        [file]
-    )
+    const { stateBrancas } = useContext (Contexto)
+    const [ brancas , setBrancas ] = stateBrancas
+    const { stateNegras } = useContext (Contexto)
+    const [ negras , setNegras ] = stateNegras
+   
+  //  Faltan por engadir:
+  //  const stateEvent = useState("")
+  //  const stateSite = useState("")
+  //  const stateDate = useState("")
+  //  const stateRound = useState("")
+  //  const stateResult = useState("")
 
-     function CambioFicheiro(e) {   
+    const { stateWhite } = useContext (Contexto)
+    const [ white , setWhite ] = stateWhite
+    const { stateBlack } = useContext (Contexto)
+    const [ black , setBlack ] = stateBlack
+
+  
+    function CambioFicheiroEsquerda(input) {   
+      const reader = new FileReader()
+      reader.addEventListener("load", (event)=>{          
+        setBrancas(event.target.result)
+        })
+      let ficheiro=input.target.files[0]
+      reader.readAsDataURL(ficheiro)
+      }
+
+      function CambioFicheiroDereita(input) {   
         const reader = new FileReader()
-        reader.readAsDataURL(e.target.files[0])
-        reader.addEventListener("load", ()=>{ 
-            setFile(reader.result)
-            })                 
-    }
+        reader.addEventListener("load", (event)=>{       
+          setNegras(event.target.result)
+          })
+        let ficheiro=input.target.files[0]
+        reader.readAsDataURL(ficheiro)
+        }
 
   return (
     <>
-    <div className={style.up}>Avatares</div>
-    <input className={style.up} type='file' onChange={CambioFicheiro}/>
+    <div className={style.centro}>
+    <form className={style.formulario}>
+      <label>Nome brancas:</label>
+      <input name='white' type='text' placeholder="nome brancas"></input>
+      <label>Nome negras:</label>
+      <input name='black' type='text' placeholder="nome negras"></input>
+      <label>Evento:</label>
+      <input name="event" type="text" placeholder='Nome do evento'></input> 
+      <label>Lugar:</label>
+      <input name="site" type="text" value='www.xadrez.es'></input>
+      <label>Rolda:</label>
+      <input name="round" type="text" value='1ª'></input> 
+      <label>Data:</label>
+      <input name="date" type="text" value='2023.06.01'></input>
+      <input id="submit" type='submit'></input> 
+    </form>
+    </div>
+
+    <div className={style.fondo}>   
+    </div>
+
+    <div className={style.esquerda}>
+    Avatar xogador brancas
+    <input type='file' onChange={CambioFicheiroEsquerda}/>
+    <div>
+    <img src={brancas} alt='b'></img>
+    </div>
+    </div> 
+    <div className={style.dereita}>
+    Avatar xogador de negras
+    <input type='file' onChange={CambioFicheiroDereita}/>
+    <div>
+    <img src={negras} alt='n'></img>
+    </div>
+    </div>
     </>
   )
 }
